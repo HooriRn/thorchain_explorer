707c2559742d4c089fa8b078d39dcd5bf906cf82
HooriRn <hoorirn@protonmail.com>
2022-02-22 21:13:33 +0330

    sidebar social icons

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -2,16 +2,38 @@
   <div class="side-bar-container">
       <img class="logo" src="@/assets/images/thorchain.png" alt="">
       <div class="side-bar-lists">
-        <button :class="[{active: index == 0}, 'side-bar-item']" v-for="(item, index) in sidebarLists" :key="index">
-          {{item.name}}
-        </button>
+        <template v-for="(item, index) in sidebarLists" >
+          <button :class="[{active: index == 0}, 'side-bar-item']" :key="index">
+            {{item.name}}
+          </button>
+        </template>
+      </div>
+      <div class="social-items">
+        <a href="https://twitter.com/THORChain">
+          <TwitterLogo class="social-icon" />
+        </a>
+        <a href="https://discord.gg/KjPVnGy5jR">
+          <DiscordLogo class="social-icon" />
+        </a>
+        <a href="https://github.com/HooriRn/thorchain_explorer">
+          <GithubLogo class="social-icon" />
+        </a>
       </div>
   </div>
 </template>
 
 <script>
+import TwitterLogo from '~/assets/images/twitter-brands.svg';
+import DiscordLogo from '~/assets/images/discord-brands.svg';
+import GithubLogo from '~/assets/images/github-brands.svg';
+
 export default {
   name: 'SideBar',
+  components: {
+    TwitterLogo,
+    DiscordLogo,
+    GithubLogo
+  },
   data() {
     return {
       sidebarLists: [
@@ -38,6 +60,10 @@ export default {
 
 <style lang="scss">
 .side-bar-container {
+  display: flex;
+  flex-direction: column;
+  min-height: 100%;
+
   .logo {
     border-radius: .3rem;
     width: 2rem;
@@ -56,7 +82,9 @@ export default {
     display: block;
     background: transparent;
     border: none;
-    margin-bottom: 1rem;
+    width: 100%;
+    padding: .5rem 0;
+    text-align: left;
     color: $mainFontColor;
 
     &:hover {
@@ -64,6 +92,23 @@ export default {
     }
 
     &.active {
+      color: #e6e6e6;
+    }
+  }
+
+  .social-items {
+    margin-top: auto;
+    display: flex;
+    justify-content: space-around;
+  }
+
+  .social-icon {
+    color: #9f9f9f;
+    cursor: pointer;
+    width: 1.5rem;
+    height: 1.5rem;
+
+    &:hover {
       color: #e6e6e6;
     }
   }
