f4670db6076d882d715a18f3d6fac79f87c5b3d8
HooriRn <hoorirn@protonmail.com>
2022-02-25 11:21:03 +0330

    new pages

---
+++ pages/network/constants/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="constants-container">
+    <stat-table :tableSettings="networkConstants"></stat-table>
+  </div>
+</template>
+
+<script>
+import StatTable from '~/components/StatTable.vue'
+export default {
+  components: { StatTable },
+  data() {
+    return {
+      networkConstans: [],
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/network/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="network-container">
+    <div style="width: 100%">
+      <stat-table :tableSettings="networkOverview"></stat-table>
+    </div>
+    <div style="height: 1rem; width: 100%"></div>
+    <div style="width: 50%">
+      <stat-table :tableSettings="topActiveBonds"></stat-table>
+    </div>
+    <div style="width: 50%">
+      <stat-table :tableSettings="topStandbyBonds"></stat-table>
+    </div>
+  </div>
+</template>
+
+<script>
+import StatTable from '~/components/StatTable.vue'
+export default {
+  components: { StatTable },
+  data() {
+    return {
+      networkOverview: [],
+      topActiveBonds: [],
+      topStandbyBonds: []
+    }
+  }
+}
+</script>
+
+<style>
+.network-container {
+  display: flex;
+}
+</style>
